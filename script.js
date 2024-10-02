588bcbf59422254a3fcb213934ca7080dd516d6b
вашAnastasiyaLastikта <katelastik@gmail.com>
2024-10-02 20:02:06 +0400

    помилка в css 02.10.2024

--- script.js
+++ script.js
@@ -1,1 +1,118 @@
+let Abissian = document.querySelector(".Abissian")
+Abissian.addEventListener("click", function(){
+    window.location.assign('Cat1.html')
+})
 
+let Bengal = document.querySelector(".Bengal")
+Bengal.addEventListener("click", function(){
+    window.location.assign('Bengal_cat.html')
+})
+
+let British = document.querySelector(".British")
+British.addEventListener("click", function(){
+    window.location.assign('British_cat.html')
+})
+
+let Ekzotuk  = document.querySelector(".Ekzotuk")
+Ekzotuk.addEventListener("click", function(){
+    window.location.assign('Ekzotuk.html')
+})
+
+let Karakal  = document.querySelector(".Karakal")
+Karakal.addEventListener("click", function(){
+    window.location.assign('Karakal.html')
+})
+
+let Manchkin  = document.querySelector(".Manchkin")
+Manchkin.addEventListener("click", function(){
+    window.location.assign('Manchkin.html')
+})
+
+let Maincun = document.querySelector(".Maincun")
+Maincun.addEventListener("click", function(){
+    window.location.assign('Maincun.html')
+})
+
+let Siamska  = document.querySelector(".Siamska")
+Siamska.addEventListener("click", function(){
+    window.location.assign('Siamska_cat.html')
+})
+
+let Snow_shu = document.querySelector(".Snow-shu")
+Snow_shu.addEventListener("click", function(){
+    window.location.assign('Snow-shu.html')
+})
+
+let Somaliiska = document.querySelector(".Somaliiska")
+Somaliiska.addEventListener("click", function(){
+    window.location.assign('Somaliiska_cat.html')
+})
+
+let Scottish = document.querySelector(".Scottish")
+Scottish.addEventListener("click", function(){
+    window.location.assign('Scottish_cat.html')
+})
+
+
+
+
+
+
+
+
+
+
+let Abissian_in_breeds = document.querySelector(".Abissian_in_breeds")
+Abissian_in_breeds.addEventListener("click", function(){
+    window.location.assign('Cat1.html')
+})
+
+let Bengal_in_breeds = document.querySelector(".Bengal_in_breeds")
+Bengal_in_breeds.addEventListener("click", function(){
+    window.location.assign('Bengal_cat.html')
+})
+
+let British_in_breeds = document.querySelector(".British_in_breeds")
+British_in_breeds.addEventListener("click", function(){
+    window.location.assign('British_cat.html')
+})
+
+let Ekzotuk_in_breeds = document.querySelector(".Ekzotuk_in_breeds")
+Ekzotuk_in_breeds.addEventListener("click", function(){
+    window.location.assign('Ekzotuk.html')
+})
+
+let Karakal_in_breeds = document.querySelector(".Karakal_in_breeds")
+Karakal_in_breeds.addEventListener("click", function(){
+    window.location.assign('Karakal.html')
+})
+
+let Manchkin_in_breeds = document.querySelector(".Manchkin_in_breeds")
+Manchkin_in_breeds.addEventListener("click", function(){
+    window.location.assign('Manchkin.html')
+})
+
+let Maincun_in_breeds = document.querySelector(".Maincun_in_breeds")
+Maincun_in_breeds.addEventListener("click", function(){
+    window.location.assign('Maincun.html')
+})
+
+let Siamska_in_breeds = document.querySelector(".Siamska_in_breeds")
+Siamska_in_breeds.addEventListener("click", function(){
+    window.location.assign('Siamska_cat.html')
+})
+
+let Snow_shu_in_breeds = document.querySelector(".Snow_shu_in_breeds")
+Snow_shu_in_breeds.addEventListener("click", function(){
+    window.location.assign('Snow-shu.html')
+})
+
+let Somaliiska_in_breeds = document.querySelector(".Somaliiska_in_breeds")
+Somaliiska_in_breeds.addEventListener("click", function(){
+    window.location.assign('Somaliiska_cat.html')
+})
+
+let Scottish_in_breeds = document.querySelector(".Scottish_in_breeds")
+Scottish_in_breeds.addEventListener("click", function(){
+    window.location.assign('Scottish_cat.html')
+})
